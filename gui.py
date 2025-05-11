1441034628985c9505fff8fada77ee338f4064cd
Basmala Ahmed Ali Ibrahim <117600939+basmala249@users.noreply.github.com>
2025-05-11 23:04:17 +0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,61 +1,53 @@
-"""
-        GUI
-"""
-import tkinter as tk
-from PIL import Image, ImageTk
-import math
+# """
+#         GUI
+# """
+# import tkinter as tk
+# from PIL import Image, ImageTk
+# import math
 import MinMax
-import AlphaBeta
+# import AlphaBeta
 
 
-stone_images = {} 
+# stone_images = {} 
 
 
 
-def DrawGrid():
-    for i in range(15):
-       canvas.create_line(0, 40 * i, 600, 40 * i)
-       canvas.create_line(40 * i, 0, 40 * i, 600)
+# def DrawGrid():
+#     for i in range(15):
+#        canvas.create_line(0, 40 * i, 600, 40 * i)
+#        canvas.create_line(40 * i, 0, 40 * i, 600)
 
-def InitializeGrid(fileName):
-    lis = []
-    with open(fileName, 'r') as file:
-         for row in file:
-             lis.append(row)
-    return lis
+# def InitializeGrid(fileName):
+#     lis = []
+#     with open(fileName, 'r') as file:
+#          for row in file:
+#              lis.append(row)
+#     return lis
 
-def PutInitialTiles():
-    global stone_images  
-    try:
-        initial_grid = InitializeGrid("initialGrid.txt")
-        for y in range(15):
-          for x in range(15):
-            cell = initial_grid[y][x] 
-            image = None
-            if cell == 'b':
-                image = Image.open("black_gm.jpg")
-            elif cell == 'w':
-                image = Image.open("white_gm.png")
-            if image:
-                resized_image = image.resize((36, 36))  
-                stone_key = f"{y}_{x}_{cell}"
-                stone_images[stone_key] = ImageTk.PhotoImage(resized_image)
-                canvas.create_image(x * 40 + 5, y * 40 + 5, anchor=tk.CENTER, image=stone_images[stone_key])
+# def PutInitialTiles():
+#     global stone_images  
+#     try:
+#         initial_grid = InitializeGrid("initialGrid.txt")
+#         for y in range(15):
+#           for x in range(15):
+#             cell = initial_grid[y][x] 
+#             image = None
+#             if cell == 'b':
+#                 image = Image.open("black_gm.jpg")
+#             elif cell == 'w':
+#                 image = Image.open("white_gm.png")
+#             if image:
+#                 resized_image = image.resize((36, 36))  
+#                 stone_key = f"{y}_{x}_{cell}"
+#                 stone_images[stone_key] = ImageTk.PhotoImage(resized_image)
+#                 canvas.create_image(x * 40 + 5, y * 40 + 5, anchor=tk.CENTER, image=stone_images[stone_key])
 
-    except Exception as e:
-        print(f"Error: {e}")
+#     except Exception as e:
+#         print(f"Error: {e}")
 
 
 
 
 def launch_game(mode) :
-    global root , canvas
-    if mode == "Human vs AI" :
-        MinMax.select_mode("HumanvsAI")
-    else :
-        root = tk.Tk()
-        root.title("Gomoku (AI Vs AI)")
-        canvas = tk.Canvas(root, width=600, height=600, bg='#E6B88C')
-        canvas.pack()  
-        AlphaBeta.Main()
+    MinMax.select_mode(mode)
 
